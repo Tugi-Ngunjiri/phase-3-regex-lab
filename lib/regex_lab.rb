6a33eeef9d0901c73f60ec90f489898cd1da15ab
tugi <blueqwinn@gmail.com>
2023-02-10 22:41:09 +0300

    first commit

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,21 @@
 def starts_with_a_vowel?(word)
-
+ puts words
+ puts ("hello")
+ puts ("hi")
+ puts ("how are you")
+ puts ("I'm fine")
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+puts text
+puts ("undoing")
+puts("unrevealing")
+puts("uninteresting")
+puts("unliving")
 end
 
 def words_five_letters_long(text)
+    puts text 
 
 end
 
